cce8124453f6487ea46e95fb1772909c376df965
Balu <baluwadave@gmail.com>
2024-06-08 13:21:32 +0530

    --- Auto Git Commit ---

---
+++ modules/vpc/variable.tf
@@ -0,0 +1,14 @@
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
+}
+
+variable "public_subnet_cidr_1a" {
+  description = "Public Subnet CIDR"
+}
+variable "public_subnet_cidr_2a" {
+  
+}
+
+variable "private_subnet_cidr" {
+  description = "Private Subnet CIDR"
+}
