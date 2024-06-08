e7fb549ce283e122c4d938b911eff508a508a686
Balu <baluwadave@gmail.com>
2024-06-08 13:21:42 +0530

    --- Auto Git Commit ---

---
+++ modules/vpc/output.tf
@@ -0,0 +1,17 @@
+output "public_subnet_1a" {
+  value = aws_subnet.public-subnet-1a.id
+  description="This is my public subnet id"
+}
+output "public_subnet_2a" {
+  value =aws_subnet.public-subnet-2a.id  
+}
+
+output "vpc_id" {
+    value = aws_vpc.terraform-vpc.id
+    description = "This is my vpc_id which i have created through s script"
+}
+
+output "subnet_id2" {
+    value = aws_subnet.private-subnet.id
+    description = "This is Private subnet id"
+}
